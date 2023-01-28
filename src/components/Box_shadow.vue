8b21343c11e52ceb0d8282706aba7992c1a7bb80
huoshicang <2717334980@qq.com>
2023-01-29 00:00:46 +0800

    引入ui框架，并更新滑块

--- src/components/Box_shadow.vue
+++ src/components/Box_shadow.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>Box_shadow</div>
+  <div>
+    <Slider v-model="value" :max="max" :on-change="change(value)"></Slider>
+  </div>
 </template>
 
 <script setup lang="ts">
 import {} from "vue";
+</script>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      value: 25,
+      max: 300,
+    };
+  },
+  onMounted: {},
+  methods: {
+    change(val: number) {
+      console.log(val);
+    },
+  },
+};
 </script>
 
 <style scoped lange="less"></style>
